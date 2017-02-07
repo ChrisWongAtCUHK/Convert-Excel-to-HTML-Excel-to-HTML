0af23929cbe6c642146d21828ca3179c1b11c509
Chris Wong <wcwong@MaximizerSoftware.onmicrosoft.com>
2017-02-07 11:24:15 +0800

    Firt commit.

---
+++ Convert-Excel-to-HTML-Excel-to-HTML/SimplestProgram.cs
@@ -0,0 +1,34 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Convert_Excel_to_HTML_Excel_to_HTML
+{
+    class SimplestProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //load Excel file
+                Workbook workbook = new Workbook();
+                workbook.LoadFromFile(@"test.xlsx");
+
+                //convert Excel to HTML
+                Worksheet sheet = workbook.Worksheets[0];
+                sheet.SaveToHtml("sample.html");
+
+                //Preview HTML
+                System.Diagnostics.Process.Start("sample.html");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
